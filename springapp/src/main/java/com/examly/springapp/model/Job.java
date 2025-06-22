e1cee1446f1c8c67f1c29a2b1c96427fe5ac7ef0
neojarvis <techadmin@iamneo.ai>
2025-06-22 15:06:00 +0000

    Auto-save-commit-6/22/2025--8:35:46-PM

---
+++ springapp/src/main/java/com/examly/springapp/model/Job.java
@@ -0,0 +1,63 @@
+package com.examly.springapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Job {
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+ private int jobId;
+ private String title;
+ private String description;
+ private String company;
+ private String location;
+  
+ public Job() {
+}
+ public Job(int jobId, String title, String description, String company, String location) {
+    this.jobId = jobId;
+    this.title = title;
+    this.description = description;
+    this.company = company;
+    this.location = location;
+}
+ public int getJobId() {
+    return jobId;
+ }
+ public void setJobId(int jobId) {
+    this.jobId = jobId;
+ }
+ public String getTitle() {
+    return title;
+ }
+ public void setTitle(String title) {
+    this.title = title;
+ }
+ public String getDescription() {
+    return description;
+ }
+ public void setDescription(String description) {
+    this.description = description;
+ }
+ public String getCompany() {
+    return company;
+ }
+ public void setCompany(String company) {
+    this.company = company;
+ }
+ public String getLocation() {
+    return location;
+ }
+ public void setLocation(String location) {
+    this.location = location;
+ }
+
+
+
+ 
+ 
+}
