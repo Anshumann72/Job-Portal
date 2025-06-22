be3bf22078018b0b6f62b9926743e6bf324aa22b
neojarvis <techadmin@iamneo.ai>
2025-06-22 15:11:58 +0000

    Manual-save-commit-6/22/2025--8:41:46-PM

--- springapp/src/main/java/com/examly/springapp/controller/JobController.java
+++ springapp/src/main/java/com/examly/springapp/controller/JobController.java
@@ -1,11 +1,21 @@
 package com.examly.springapp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.*;
+import com.examly.springapp.model.Job;
 @RestController
 public class JobController {
 
 
+     @GetMapping("api/test/job")
+    public ArrayList<Job> getJob(){
+        ArrayList<Job> jobs=new ArrayList<>();
+        jobs.add(new Job(1,"SWE","Spring Boot Development","LTI","Banglore"));
+        return jobs;
 
-    
+    }
+
+
+
 }
