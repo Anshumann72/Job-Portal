1f6c8a5ecea2ac3fc634a8ed230dba33816ec1e5
neojarvis <techadmin@iamneo.ai>
2025-06-26 06:10:31 +0000

    Auto-save-commit-6/26/2025--11:40:15-AM

--- springapp/src/main/java/com/examly/springapp/controller/JobController.java
+++ springapp/src/main/java/com/examly/springapp/controller/JobController.java
@@ -1,13 +1,23 @@
 package com.examly.springapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.*;
 import com.examly.springapp.model.Job;
+import com.examly.springapp.service.JobService;
 @RestController
 public class JobController {
 
 
+    @Autowired
+    JobService jobService;
      @GetMapping("api/test/job")
     public ArrayList<Job> getJob(){
         ArrayList<Job> jobs=new ArrayList<>();
@@ -15,6 +25,19 @@ public class JobController {
         return jobs;
 
     }
+
+    @PostMapping("api/jobs")
+    public ResponseEntity<?> addJobs(@RequestBody Job job){
+        Job j1=jobService.addJobs(job);
+        return new ResponseEntity<>(j1,HttpStatus.valueOf(201));
+    }
+
+    @PutMapping("api/jobs/{jobsId}")
+    public ResponseEntity<?> updateJobs(@PathVariable int jobId,@RequestBody Job job){
+        Job j1=jobService.updateJobs(job);
+        return new ResponseEntity<>(j1,HttpStatus.valueOf(200));
+    }
+
 
 
 
--- springapp/src/main/java/com/examly/springapp/service/JobService.java
+++ springapp/src/main/java/com/examly/springapp/service/JobService.java
@@ -1,5 +1,17 @@
 package com.examly.springapp.service;
 
+import com.examly.springapp.model.Job;
+
 public class JobService {
+
+    public Job addJobs(Job job) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addJobs'");
+    }
+
+    public Job updateJobs(Job job) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateJobs'");
+    }
 
 }
