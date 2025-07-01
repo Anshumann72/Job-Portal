2e86fab59ba9a6c51144c24b786d4f1baf1acd21
neojarvis <techadmin@iamneo.ai>
2025-07-01 16:06:24 +0000

    Submit-project-save-commit-7/1/2025--9:36:07-PM

--- springapp/src/main/java/com/examly/springapp/service/ApplicationService.java
+++ springapp/src/main/java/com/examly/springapp/service/ApplicationService.java
@@ -1,5 +1,15 @@
 package com.examly.springapp.service;
 
+import java.util.List;
+
+import com.examly.springapp.model.Application;
+
 public interface ApplicationService {
+
+    List<Application> getAllApplications();
+    Application getApplicationById();
+    Application saveApplication(Application application);
+    Application updateApplication(int id,Application application);
+    void deleteApplication(int id);
 
 }
