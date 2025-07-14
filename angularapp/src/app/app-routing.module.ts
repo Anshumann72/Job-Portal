3a8c030c492c3b551c3f02981b7205613ec2becb
neojarvis <techadmin@iamneo.ai>
2025-07-14 04:42:49 +0000

    Submit-project-save-commit-7/14/2025--10:12:10-AM

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddJobComponent } from './components/add-job/add-job.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'add-job',component:AddJobComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
