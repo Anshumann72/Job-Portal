27e5d8f6804d1c31a64c43c7a04912cbeabdeb5f
neojarvis <techadmin@iamneo.ai>
2025-07-14 04:48:52 +0000

    Submit-project-save-commit-7/14/2025--10:18:27-AM

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddJobComponent } from './components/add-job/add-job.component';
+import { JobListComponent } from './components/job-list/job-list.component';
+import { ViewApplicationComponent } from './components/view-application/view-application.component';
 
-const routes: Routes = [{
-  path:'add-job',component:AddJobComponent
-}];
+const routes: Routes = [
+  {path:'add-job',component:AddJobComponent},
+  {path:'job-list', component:JobListComponent},
+  {path:'view-all-application',component:ViewApplicationComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
