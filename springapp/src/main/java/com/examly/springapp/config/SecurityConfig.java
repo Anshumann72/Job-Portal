2a192dab498883f09a1846fd80aabec5211b44cc
neojarvis <techadmin@iamneo.ai>
2025-07-19 08:59:45 +0000

    Manual-save-commit-7/19/2025--2:29:16-PM

--- springapp/src/main/java/com/examly/springapp/config/SecurityConfig.java
+++ springapp/src/main/java/com/examly/springapp/config/SecurityConfig.java
@@ -1,36 +1,82 @@
-//SECURITY CONFIG
+	
 
 package com.examly.springapp.config;
 
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-
 public class SecurityConfig {
+    @Autowired
+
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http
-            .csrf(csrf->csrf.disable())
-            .authorizeHttpRequests(auth->auth
-            .anyRequest().permitAll()
-            )
-            .httpBasic();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+        .cors(cors -> {
+                    cors.configurationSource(new CorsConfigurationSource() { 
+                        @Override
+                        public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                            CorsConfiguration cfg = new CorsConfiguration();
+
+                            cfg.setAllowedOriginPatterns(Collections.singletonList("*"));
+                            cfg.setAllowedOriginPatterns(Collections.singletonList("https://8081-aeecdbdbcb329136753cdfbcccefone.project.examly.io"));
+                            cfg.setAllowedMethods(Collections.singletonList("*"));
+        
+                            cfg.setAllowCredentials(true);
+                            cfg.setAllowedHeaders(Collections.singletonList("*"));
+                            cfg.setExposedHeaders(Arrays.asList("Authorization"));
+
+                            return cfg;
+
+                        }
+                    });
+                })
+
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/user/login",
+                                "/api/user/register")
+                        // .anyRequest()
+                        .permitAll()
+                        .anyRequest().permitAll())
+                .exceptionHandling(ex -> ex.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
--- springapp/src/main/java/com/examly/springapp/controller/AuthController.java
+++ springapp/src/main/java/com/examly/springapp/controller/AuthController.java
@@ -1,4 +1,3 @@
-
 package com.examly.springapp.controller;
 
 import java.util.Map;
