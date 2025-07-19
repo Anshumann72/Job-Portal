06af777681ca1bdbe22dfe26ba89ae6a8858ff95
neojarvis <techadmin@iamneo.ai>
2025-07-19 08:21:22 +0000

    Auto-save-commit-7/19/2025--1:50:53-PM

---
+++ springapp/src/main/java/com/examly/springapp/config/MyUserDetailsService.java
@@ -0,0 +1,35 @@
+
+//MYUSER DETAILS
+
+package com.examly.springapp.config;
+
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.examly.springapp.model.User;
+import com.examly.springapp.repository.UserRepo;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService{
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        
+        User user = userRepo.findByUsername(username).orElseThrow(()->new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(
+            user.getUsername(),
+            user.getPassword(),
+            Collections.singletonList(new SimpleGrantedAuthority("ROLE_"+ user.getRole()))
+        );
+        
+    }
+}
