036d40185d20294b587c42682912f5f6d8a86869
neojarvis <techadmin@iamneo.ai>
2025-07-20 12:53:42 +0000

    Submit-project-save-commit-7/20/2025--6:23:14-PM

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # deefdaafcac329136918cdfbcccefone
 https://sonarcloud.io/summary/overall?id=iamneo-production_deefdaafcac329136918cdfbcccefone
-
-
-
-
-
--- angularapp/src/app/components/add-job/add-job.component.ts
+++ angularapp/src/app/components/add-job/add-job.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-add-job',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-job.component.css']
 })
 export class AddJobComponent implements OnInit {
+  job: any = {};
+  successMessage: string = '';
+  errorMessage: string = '';
+  isEditMode: boolean = false;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    const jobId = this.route.snapshot.paramMap.get('id');
+    if (jobId) {
+      this.isEditMode = true;
+      this.loadJobDetails(+jobId);
+    }
+  }
+
+  loadJobDetails(jobId: number): void {
+    // Placeholder for job loading logic
+    console.log('Loading job details for ID:', jobId);
+  }
+
+  saveJob(): void {
+    if (this.isEditMode) {
+      this.successMessage = 'Job updated successfully!';
+    } else {
+      this.successMessage = 'Job added successfully!';
+      this.clearForm();
+    }
+  }
+
+  clearForm(): void {
+    this.job = {};
   }
 
 }
