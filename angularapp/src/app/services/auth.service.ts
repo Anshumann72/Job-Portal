b134d90e4ef5d4923f60fcdf0af7307e8c5a9933
neojarvis <techadmin@iamneo.ai>
2025-07-22 17:19:58 +0000

    Auto-save-commit-7/22/2025--10:49:26-PM

--- angularapp/src/app/services/auth.service.ts
+++ angularapp/src/app/services/auth.service.ts
@@ -1,9 +1,64 @@
+
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  baseUrl: string = 'https://8080-deefdaafcac329136918cdfbcccefone.project.examly.io/api/user';  // replace with your url
 
-  constructor() { }
+  private authStatus = new BehaviorSubject<boolean>(this.hasToken());
+  private userRoleSubject = new BehaviorSubject<string | null>(localStorage.getItem('userRole'));
+  private userIdSubject = new BehaviorSubject<number | null>(+localStorage.getItem('userId')!);
+
+  constructor(private http: HttpClient) { }
+
+  private hasToken(): boolean {
+    return !!localStorage.getItem('token');
+  }
+
+  login(credentials: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/login`, credentials);
+  }
+
+  register(data: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/register`, data);
+  }
+
+  logout(): void {
+    localStorage.clear();
+    this.authStatus.next(false);
+    this.userRoleSubject.next(null);
+    this.userIdSubject.next(null);
+  }
+
+  isAuthenticated(): boolean {
+    return !!localStorage.getItem('token');
+  }
+
+  getAuthStatus(): Observable<boolean> {
+    return this.authStatus.asObservable();
+  }
+
+  getUserId(): number | null {
+    return this.userIdSubject.value;
+  }
+
+  getUserRole(): string | null {
+    return this.userRoleSubject.value;
+  }
 }
+
+
+
+// import { Injectable } from '@angular/core';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class AuthService {
+
+//   constructor() { }
+// }
--- angularapp/src/app/services/job.service.ts
+++ angularapp/src/app/services/job.service.ts
@@ -1,9 +1,61 @@
+
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JobService {
 
-  constructor() { }
+  // public baseUrl = 'https://ide-dcdaebcc328808007cdfbcccefone.project.examly.io/proxy/8080/api/jobs';
+
+   baseUrl = 'https://8080-deefdaafcac329136918cdfbcccefone.project.examly.io/api/jobs';  // replace with your url
+   
+
+   
+  //  baseUrl = "http://localhost:8080/api/jobs";  
+
+  constructor(private http: HttpClient) { }
+
+  getAllJobs(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}`).pipe(catchError(this.handleError));
+  }
+
+  getJobById(id: number): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/${id}`).pipe(catchError(this.handleError));
+  }
+
+  getJobsByUserId(userId: number): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/user/${userId}`).pipe(catchError(this.handleError));
+  }
+
+  createJob(job: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}`, job).pipe(catchError(this.handleError));
+  }
+
+  updateJob(id: number, job: any): Observable<any> {
+    return this.http.put(`${this.baseUrl}/${id}`, job).pipe(catchError(this.handleError));
+  }
+
+  deleteJob(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`).pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: any) {
+    return throwError(() => error);
+  }
 }
+
+
+// import { Injectable } from '@angular/core';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class JobService {
+
+//   constructor() { }
+// }
+
