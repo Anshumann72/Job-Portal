01ca63335cc9e82a7631fb7a571b1e3a82078acb
neojarvis <techadmin@iamneo.ai>
2025-07-23 05:34:27 +0000

    Auto-save-commit-7/23/2025--11:03:57-AM

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # deefdaafcac329136918cdfbcccefone
 https://sonarcloud.io/summary/overall?id=iamneo-production_deefdaafcac329136918cdfbcccefone
 
-
-
--- springapp/src/main/java/com/examly/springapp/config/JwtUtils.java
+++ springapp/src/main/java/com/examly/springapp/config/JwtUtils.java
@@ -1,8 +1,7 @@
 
 package com.examly.springapp.config;
 
-
-
+import java.security.Key;
 import java.util.Date;
 
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,24 +9,29 @@ import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtUtils {
 
-    private final String jwtSecret = "SECRET_KEY";
+    public static final String jwtSecret ="secretsfhsfjhdkjngdfjkgfgjdlkfjsdkfjsd";
+	public static final String JWT_HEADER = "Authorization";
+    //private final String jwtSecret = "SECRET_KEY";
     private final int jwtExpirationMs = 86400000;
-
+    Key key = Keys.hmacShaKeyFor(jwtSecret.getBytes()); //new line
+    // @SuppressWarnings("deprecation")
     public String generateToken(UserDetails userDetails){
         return Jwts.builder()
             .setSubject(userDetails.getUsername())
             .setIssuedAt(new Date())
             .setExpiration(new Date(System.currentTimeMillis()+jwtExpirationMs))
-            .signWith(SignatureAlgorithm.HS512,jwtSecret)
+            // .signWith(SignatureAlgorithm.HS512,jwtSecret)
+            .signWith(key)
             .compact();
     }
 
     public String extractUsername(String token){
-        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+        return Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody().getSubject();
     }
 
     public boolean validateToken(String token,UserDetails userDetails){
@@ -36,7 +40,8 @@ public class JwtUtils {
     }
 
     public boolean isTokenExpired(String token){
-        Date expiration = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getExpiration();
+        Date expiration = Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody().getExpiration();
         return expiration.before(new Date());
     }
 }
+
